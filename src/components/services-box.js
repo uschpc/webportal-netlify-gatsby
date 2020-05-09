ee67189daa3ef0cfc2bed0bb21766a29deb0280a
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2020-05-09 11:04:33 -0700

    Updating the 4 boxes and adding 3 tiles below that

--- src/components/services-box.js
+++ src/components/services-box.js
@@ -6,12 +6,104 @@ const ServicesBox = () => {
             <div class="services-block">
             <div className="view view-services">
                 <div className="view-content">
-                    <ul>
-                    <a href="#" class="btn-flip" data-back="Our department has helped enable the advancement of critical inquiry" data-front="Data Visualization"></a>
+                    <div id="x-section-5" class="x-section">
+                        <div className="marginless-columns" >
+                            <div className="shared">
+                                <a href="#">
+                                    <span className="features">Research</span>
+                                    <span className="copy-text">Some text here</span>
+                                </a>
+                            </div>
+                            <div className="shared">
+                                <a href="#">
+                                    <span className="features">Innovation</span>
+                                    <span className="copy-text">Some text here</span>
+                                </a>
+                            </div>
+                            <div className="shared">
+                                <a href="#">
+                                    <span className="features">Training</span>
+                                    <span className="copy-text">some test here</span>
+                                </a>
+                            </div>
+                            <div className="shared">
+                                <a href="#">
+                                    <span className="features">Hight Computing</span>
+                                    <span className="copy-text">Some text here</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="content-container">
+                        <div className="shared press-release">
+                            <h2>Featured Story</h2>
+                            <div className="press-news-block">
+                                <div className="block">
+                                    <h3 className="title">A Business-Savvy Storyteller</h3>
+                                    <img src="https://iamachild.files.wordpress.com/2013/06/the-storyteller1.jpg"></img>
+                                    <h4 className="copy-text">From a remote island campus, <a href="#">Read More</a></h4>
+                                </div>
+                                
+                                <div className="block">
+                                    <img src="https://i.ytimg.com/vi/kdw2A9ZpoTo/maxresdefault.jpg"></img>
+                                    <h3 className="title">CONFLUX</h3>
+                                    <h4 className="copy-text">Combining Big Data and HPC, <a href="#">Read More</a></h4>
+                                </div>
+                            </div>
+                            </div>
+                        <div className="shared news">
+                            <h2>Latest News</h2>
+                            <div className="press-news-block">
+                                <div className="block">
+                                    <h3 className="title">MSI PIs Elected Fellows of the American Academy of Arts and Sciences</h3>
+                                    <img src="https://blog.netapp.com/wp-content/uploads/2018/08/Twitter-Image-1024x512.jpg"></img>
+                                    <h4 className="copy-text">Two MSI PIs have been elected to as Fellows of the American Academy, <a href="#">Read More</a></h4>
+                                </div>
+                                
+                                <div className="block">
+                                    <img src="https://www.stulz-usa.com/fileadmin/_processed_/b/2/csm_STULZ-Micro-Data-Center-Press-Release_0fe6a42bab.jpg"></img>
+                                    <h3 className="title">May 2020 Maintenance</h3>
+                                    <h4 className="copy-text">A global system reservation will start at 5:00 a.m. on May 6. Jobs that cannot be completed before 5:00 a.m. on May 6 will, <a href="#">Read More</a></h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="shared system-status">
+                            <h2>Upcoming Events</h2>
+                            <div className="postcard-left">
+                                <div className="date-stacked">
+                                    <div className="date-month"><span className="date-display-single">May</span></div>
+                                    <div className="date-day"><span className="date-display-single">07</span></div>
+                                </div>
+                                <div className="event-title normal-link"><h3><a href="#">Sherlock Zoom office hours</a></h3></div>
+                                <div className="event-location descriptor"></div>
+                                <div className="event-date-long descriptor"><span class="date-display-single">3:00pm</span></div>
+                            </div>
+                            <div className="postcard-left">
+                                <div className="date-stacked">
+                                    <div className="date-month"><span className="date-display-single">May</span></div>
+                                    <div className="date-day"><span className="date-display-single">07</span></div>
+                                </div>
+                                <div className="event-title normal-link"><h3><a href="#">Sherlock Zoom office hours</a></h3></div>
+                                <div className="event-location descriptor"></div>
+                                <div className="event-date-long descriptor"><span class="date-display-single">3:00pm</span></div>
+                            </div>
+                            <div className="postcard-left">
+                                <div className="date-stacked">
+                                    <div className="date-month"><span className="date-display-single">May</span></div>
+                                    <div className="date-day"><span className="date-display-single">07</span></div>
+                                </div>
+                                <div className="event-title normal-link"><h3><a href="#">Sherlock Zoom office hours</a></h3></div>
+                                <div className="event-location descriptor"></div>
+                                <div className="event-date-long descriptor"><span class="date-display-single">3:00pm</span></div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <ul> */}
+                    {/* <a href="#" class="btn-flip" data-back="Our department has helped enable the advancement of critical inquiry" data-front="Data Visualization"></a>
                     <a href="#" class="btn-flip" data-back="Formats a HTML string/file with your desired indentation level. The formatting rules are not configurable" data-front="Training &amp; Education"></a>
                     <a href="#" class="btn-flip" data-back="Formats a HTML string/file with your desired indentation level. The formatting rules are not configurable
                                     Note that the formatter" data-front="High Performance Computing"></a>
-                    <a href="#" class="btn-flip" data-back="Formats a HTML string/file with your desired indentation level" data-front="Enabling Research"></a>
+                    <a href="#" class="btn-flip" data-back="Formats a HTML string/file with your desired indentation level" data-front="Enabling Research"></a> */}
 
                     {/* <li className="views-row">
                         <img className="service-box-icon" src="/images/books-100.png" />
@@ -56,7 +148,7 @@ const ServicesBox = () => {
                             </div>
                         </a>
                     </li> */}
-                    </ul>
+                    {/* </ul> */}
                 </div>
             </div>
         </div>
