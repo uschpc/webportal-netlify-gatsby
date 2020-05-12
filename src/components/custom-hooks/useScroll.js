8fb22b2d295c7f2903e36f605372d33c761386d0
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2020-05-11 20:02:14 -0700

    Adding a condition for the document tag

--- src/components/custom-hooks/useScroll.js
+++ src/components/custom-hooks/useScroll.js
@@ -9,14 +9,14 @@ import React, { useState, useEffect } from "react";
 export function useScroll() {
   const [lastScrollTop, setLastScrollTop] = useState(0);
   const [bodyOffset, setBodyOffset] = useState(
-    document.body.getBoundingClientRect()
+    document ? document.body.getBoundingClientRect() : 0
   );
   const [scrollY, setScrollY] = useState(bodyOffset.top);
   const [scrollX, setScrollX] = useState(bodyOffset.left);
   const [scrollDirection, setScrollDirection] = useState();
 
   const listener = e => {
-    setBodyOffset(document.body.getBoundingClientRect());
+    setBodyOffset(document ? document.body.getBoundingClientRect() : 0);
     setScrollY(-bodyOffset.top);
     setScrollX(bodyOffset.left);
     setScrollDirection(lastScrollTop > -bodyOffset.top ? "down" : "up");
