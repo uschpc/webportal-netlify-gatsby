27eb54c21c0063c8fed66d3f26b00eaf6f4828ab
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2020-05-12 17:50:48 -0700

    Remove User support sub menu list

--- src/helper/DropDowns.js
+++ src/helper/DropDowns.js
@@ -163,39 +163,42 @@ const EducationOutreachSubNavDropdown = ({ current, prev }) => {
 
 const UserSupportSubNavDropdown = ({ current, prev }) => {
   let subNav = assignedDropdownSubNav('User Support');
-
-  return (
-    <div className="products-dropdown-el dropdown-el" data-current={current} data-prev={prev}>
-      <div className="dropdown-content">  
-      <div className="row">
-        <div className="column">
-          <h4>Life is beautiful</h4>         
-        </div>
-        <div className="column">
-          <ul>
-          {subNav.sub_nav_items.map((item, i) => {
-          return (
-            <li>
-              <Router>
-                <Link className="heading" to={item.path}><h3 className="heading">{item.label}</h3></Link>
-              </Router>
-              </li>
-            )})
-          }
-        </ul>
-        </div>
-        <div className="column">
-        <h4>Related Links</h4>
-          <div className="links">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+  if(subNav.sub_nav_items > 0) {
+    return (
+      <div className="products-dropdown-el dropdown-el" data-current={current} data-prev={prev}>
+        <div className="dropdown-content">  
+        <div className="row">
+          <div className="column">
+            <h4>Life is beautiful</h4>         
+          </div>
+          <div className="column">
+            <ul>
+            {subNav.sub_nav_items.map((item, i) => {
+            return (
+              <li>
+                <Router>
+                  <Link className="heading" to={item.path}><h3 className="heading">{item.label}</h3></Link>
+                </Router>
+                </li>
+              )})
+            }
+          </ul>
+          </div>
+          <div className="column">
+          <h4>Related Links</h4>
+            <div className="links">
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+            </div>
           </div>
         </div>
-      </div>
-  </div>
     </div>
-  );
+      </div>
+    );
+  } else {
+    return ''
+  }
 }
 
 const NewsEventsSubNavDropdown = ({ current, prev }) => {
