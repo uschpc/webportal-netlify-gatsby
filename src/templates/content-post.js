20159d3ea6066f18f9a55e65098549dc1a40a038
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2020-05-20 18:00:05 -0700

    Fix reloading issue

--- src/templates/content-post.js
+++ src/templates/content-post.js
@@ -5,25 +5,30 @@ import SEO from '../components/seo'
 import Footer from '../components/footer'
 
 export default function Template({ data }) {
-  const pathName = typeof window !== 'undefined' ? window.location.pathname.split('/')[1] : 'people';
-  const filterdPost = data.md.edges
+  const pathName = typeof window !== 'undefined' ? window.location.pathname.split('/')[1] : '';
+  if (pathname !== '') {
+    const filterdPost = data.md.edges
         .filter(edge => edge.node.frontmatter.path === pathName)
-  const post = filterdPost[0].node;
-  
+    const post = filterdPost[0].node;
+    
 
-  return (
-    <Layout {...data.navigation}>
-        <SEO title={post.frontmatter.title}/>
-        <div className="pages-container">
-            <h1>{post.frontmatter.title}</h1>
-            <h4>
-                Posted by {post.frontmatter.author} on {post.frontmatter.date}
-            </h4>
-            <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        </div>
-        <Footer />
-    </Layout>
-  )
+    return (
+      <Layout {...data.navigation}>
+          <SEO title={post.frontmatter.title}/>
+          <div className="pages-container">
+              <h1>{post.frontmatter.title}</h1>
+              <h4>
+                  Posted by {post.frontmatter.author} on {post.frontmatter.date}
+              </h4>
+              <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          </div>
+          <Footer />
+      </Layout>
+    )
+  } else {
+    return ''
+  }
+  
 }
 
 export const postQuery = graphql`
