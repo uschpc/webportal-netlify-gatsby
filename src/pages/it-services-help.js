4a600ebb0a48a84b2ca4a380d674fc951d496d68
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2020-06-10 08:37:12 -0700

    Adding it help page with ifram

---
+++ src/pages/it-services-help.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Footer from "../components/footer.js";
+import SharedTemplate from "../components/sharedTemplate"
+
+const UserSupportIndex = ({data}) => {
+  return (
+      <Layout {...data.navigation}>
+          <SEO title="user-support" />
+          <iframe id="tickets" src="https://hpcaccount.usc.edu/static/web/supportform.php"></iframe>
+          <Footer />
+      </Layout>
+  )
+}
+
+export default UserSupportIndex
+
+export const pageQuery = graphql`
+    query{
+      navigation: allMarkdownRemark(filter: {frontmatter: {cat: {eq: "navigation"}}}) {
+        edges {
+          node {
+            frontmatter {
+              path
+              parentPath
+              title
+              parentEle
+            }
+          }
+        }
+      }
+    }
+`
