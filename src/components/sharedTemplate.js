e7d6b6c1f4e56fc6016010d8fe28d252a348f711
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2020-07-29 21:30:46 -0700

    Trying lazy loading - 5

--- src/components/sharedTemplate.js
+++ src/components/sharedTemplate.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'gatsby';
 import Markdown from "react-markdown"
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
