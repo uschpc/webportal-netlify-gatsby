53076cbbfc7043f1c7e4df6dd6a681d73b0a277c
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2020-08-28 15:30:20 -0700

    Updaye newsletter

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,9 +5,8 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, { useEffect, useState } from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
-import {Helmet} from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
@@ -15,9 +14,9 @@ import "./layout.css"
 import "../style.less"
 import PopUp from "./draggable";
 import "../draggable.scss"
+import {Helmet} from "react-helmet";
 
 const Layout = (props) => {
-  const [newsLetter, setFlag] = useState(false)
 
   useEffect(() => {
     let externalLinks = document.querySelectorAll("a[href^='http']");
@@ -42,7 +41,6 @@ const Layout = (props) => {
   }, [])
 
   useEffect(() => {
-    setFlag(true)
     if (props.backToTopBtnFlag) {
       const scrollToTopButton = document.getElementById('js-top');
 
@@ -86,12 +84,9 @@ const Layout = (props) => {
 
   return (
     <>
-    {newsLetter && (
-       <Helmet>
-       <script id="mcjs">{!function(c,h,i,m,p){ return (m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p))}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/1bdd19e9fa2d811ef66b3485a/274284bf0b2cd2f1ec24e01e7.js")}</script>
+     <Helmet>
+          <script src="https://chimpstatic.com/mcjs-connected/js/users/1bdd19e9fa2d811ef66b3485a/274284bf0b2cd2f1ec24e01e7.js"></script>
       </Helmet>
-    )}
-    
       <Header siteTitle={data.site.siteMetadata.title} searchData={(e) => props.searchData(e)} nav={props.edges} uniqId={props.uniqId} backToTopBtnFlag={props.backToTopBtnFlag} />
       <div>
         <main>{props.children}</main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,26 @@ import SEO from "../components/seo"
 import Carsoul from "../components/slider.js";
 import BodyContent from "../components/body-content.js";
 import Footer from "../components/footer.js";
+import {Helmet} from "react-helmet";
+
 
 const IndexPage = (props) => {
+  const [newsLetter, setFlag] = useState(false)
   const [model, setModelFlag] = useState(false)
   const openModel = () => {
     setModelFlag(!model)
   }
+  useEffect(() => {
+    setFlag(true)
+  })
 
   return (
       <Layout {...props.data.navigation} openModel={model}>
+         {newsLetter && (
+          <Helmet>
+          <script id="mcjs">{!function(c,h,i,m,p){ return (m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p))}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/1bdd19e9fa2d811ef66b3485a/274284bf0b2cd2f1ec24e01e7.js")}</script>
+          </Helmet>
+        )}
           <SEO title="Home" />
           <Carsoul />
           <BodyContent {...props.data} openModel={openModel} />
