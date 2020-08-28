6b1b9eadb98381bddd0454e8a1eb2935e0ef48dd
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2020-08-28 08:23:49 -0700

    pop up news letter - testing

--- src/components/layout.js
+++ src/components/layout.js
@@ -84,7 +84,8 @@ const Layout = (props) => {
   return (
     <>
      <Helmet>
-        <script id="mcjs">{!function(c,h,i,m,p){ return m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/1bdd19e9fa2d811ef66b3485a/274284bf0b2cd2f1ec24e01e7.js")}</script>
+       <script src="https://chimpstatic.com/mcjs-connected/js/users/1bdd19e9fa2d811ef66b3485a/274284bf0b2cd2f1ec24e01e7.js"></script>
+        <script id="mcjs">{!function(c,h,i,m,p){ return m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}}</script>
       </Helmet>
       <Header siteTitle={data.site.siteMetadata.title} searchData={(e) => props.searchData(e)} nav={props.edges} uniqId={props.uniqId} backToTopBtnFlag={props.backToTopBtnFlag} />
       <div>
