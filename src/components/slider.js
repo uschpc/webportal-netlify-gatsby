978b6f716591d100109be788c852ad28e7eff459
Andrea Renney <andrearenney@CAL-MBP-Andrea.lan>
2020-09-02 13:04:39 -0700

    Updated slider photos

--- src/components/slider.js
+++ src/components/slider.js
@@ -4,30 +4,30 @@ import 'react-animated-slider/build/horizontal.css';
 import { Link } from 'gatsby';
 
 const content = [
-    {
-      title: "USC's Center for Advanced Research Computing (CARC) Officially Launches",
-      description:
-        "Name change from HPC highlights the research-focused nature of the department",
-      button: "Read More",
-      url: "/news-and-events/news-and-announcements/carc-name-change",
-      image: "/images/slider-image-4-small.jpg"
-    },
-    {
-      title: "Exploring New Features on the CARC Website",
-      description:
-        "New website has an updated design and many new features for users",
-      button: "Read More",
-      url: "/news-and-events/news-and-announcements/website-new-features",
-      image: "/images/slider-image-1-small.jpg"
-    },
-    {
-      title: "Upcoming Changes for USC Research Computing: Summer and Fall 2020",
-      description:
-        "Technical upgrades, increased user support, and exciting new services are planned for the next few months",
-      button: "Read More",
-      url: "/news-and-events/news-and-announcements/upcoming-changes-summer-fall-2020",
-      image: "/images/slider-image-2-small.jpg"
-    }
+  {
+    title: "USC's Center for Advanced Research Computing (CARC) Officially Launches",
+    description:
+      "Name change from HPC highlights the research-focused nature of the department",
+    button: "Read More",
+    url: "/news-and-events/news-and-announcements/carc-name-change",
+    image: "/images/slider-image-2-small.jpg"
+  },
+  {
+    title: "Exploring New Features on the CARC Website",
+    description:
+      "New website has an updated design and many new features for users",
+    button: "Read More",
+    url: "/news-and-events/news-and-announcements/website-new-features",
+    image: "/images/slider-image-1-small.jpg"
+  },
+  {
+    title: "Upcoming Changes for USC Research Computing: Summer and Fall 2020",
+    description:
+      "Technical upgrades, increased user support, and exciting new services are planned for the next few months",
+    button: "Read More",
+    url: "/news-and-events/news-and-announcements/upcoming-changes-summer-fall-2020",
+    image: "/images/slider-image-4-small.jpg"
+  }
   ];
 
 const Carsoul = () => {
