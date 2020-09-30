8b808a04f2f0b03ed5551a204e4ed3358e07a028
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2020-09-29 20:57:05 -0700

    Update zoom meeting logic to use two endpoints

--- src/components/zoom-meeting.js
+++ src/components/zoom-meeting.js
@@ -6,7 +6,7 @@ import Loader from 'react-loader-spinner'
 
 
 const ZoomMeeting = ({html}) => {
-    const [meetings, fetchMeetings] = useState(null)
+    const [meetingResults, fetchMeetings] = useState(null)
 
     const formatDate = (time) => {
         let [month, date, year]    = ( new Date(time) ).toLocaleDateString().split("/")
@@ -20,25 +20,57 @@ const ZoomMeeting = ({html}) => {
     }
 
     useEffect(() => {
-        axios.get(`https://hpcaccount.usc.edu/static/zoom/zoom_listmeetings.php`).then( res => fetchMeetings(res.data.meetings) )
+        (async () => {
+            let resultArray = []
+            let promissesArray = []
+            let promiss = null
+            let result = await axios.get(`https://hpcaccount.usc.edu/static/zoom/zoom_listmeetings.php`)
+            let meetings = result.data.meetings 
+            meetings.forEach(({id}, index) => {
+                promiss = axios.get(`https://hpcaccount.usc.edu/static/zoom/zoom_getmeeting.php?id=${id}`)
+                promissesArray.push(promiss)    
+                // meetings[index].occurrences = res.data.occurrences
+            })
+            let meetingResults = await Promise.all(promissesArray)
+            meetingResults.forEach(meeting => {
+                resultArray.push(meeting.data)
+            })
+            fetchMeetings(resultArray)
+        })()
     }, [])
+
+    // useEffect(() => {
+    //     let process = null
+    //     let promissesArray = []
+    //     meetings.length && meetings.forEach(({id}) => {
+    //         axios.get(`https://hpcaccount.usc.edu/static/zoom/zoom_getmeeting.php?id=${id}`).then( res => console.log('coco', res) )
+    //     })
+    // }, flag)
     return (
-        meetings ? (
+        meetingResults ? (
             <>
             <Markdown source={html} escapeHtml={false} />
             <h2 className="meetings-heading">USC Advanced Research Computing - Upcoming Events</h2>
-            {meetings.map((meeting, index) => {
+            {meetingResults.map((meeting, index) => {
+                console.log('inside', meetingResults)
                 return (
-                    <div className={`meetings ${(meetings.length - 1) === index ? 'last': ''}`} key={index}>
+                    <div className={`meetings ${(meetingResults.length - 1) === index ? 'last': ''}`} key={index}>
                         <h3 className="topic">{meeting.topic}</h3>
                         <div className="agenda">{meeting.agenda}</div>
                         <div className="meeting-info">
                             <div className="left-meeting-column">
-                                <div className="date"><span>Date:</span> {formatDate(meeting.start_time)}</div>
-                                <div className="time"><span>Time:</span> {formatTime(meeting.start_time)}</div>
+                                <div className="date"><span>Date:</span> {
+                                meeting.occurrences ? 
+                                meeting.occurrences.map(oc => {
+                                    return <li>{formatDate(oc.start_time)}</li>
+                                        }
+                                        ) : 
+                                        formatDate(meeting.start_time) }
+                                    </div>
+                                <div className="time"><span>Time:</span> {formatTime(meeting.start_time ? meeting.start_time : meeting.occurrences[0].start_time)}</div>
                             </div>
                             <div className="middile-meeting-column">
-                                <div className="duration"><span>Duration:</span> {meeting.duration} minutes</div>
+                                <div className="duration"><span>Duration:</span> {meeting.duration ? meeting.duration : meeting.occurrences[0].duration} minutes</div>
                                 <div className="timezone"><span>Timezone:</span> {meeting.timezone}</div>
                             </div>
                         </div>
