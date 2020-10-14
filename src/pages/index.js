cedf467635f6bdbbb365e1d8e7bc506295301e1f
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2020-10-14 05:41:22 -0700

    not showing pas due event zoom and update images quality

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,7 +75,7 @@ export const pageQuery = graphql`
               thumbnail
               featuredImage {
                 childImageSharp {
-                  fluid(fit: COVER, maxHeight: 262) {
+                  fluid(maxHeight: 450) {
                     src
                   }
                 }
